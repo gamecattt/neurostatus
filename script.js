b5b7ae8df3987d435f8d876c7168a1cfdb35fed3
gamecattt <ak19gamecat@gmail.com>
2022-08-22 17:47:53 +0300

    script

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-document.querySelector('.faq__heading').addEventListener('click', function () {});
+// document.querySelector(".faq__heading").addEventListener("click", function () {
+//     document.querySelector(".faq__item").classList.add("active");
+// });
+
+// document.querySelectorAll(".faq__heading").forEach(function (el) {
+//     el.addEventListener("click", function () {
+//         document.querySelector(".faq__item").classList.add("active");
+//     });
+// });
+
+document.querySelectorAll(".faq__item").forEach(function (faq__item) {
+    faq__item.querySelector(".faq__heading").addEventListener("click", function () {
+        if (faq__item.classList.contains("active")) {
+            faq__item.classList.remove("active");
+        } else {
+            faq__item.classList.add("active");
+        }
+    });
+});
