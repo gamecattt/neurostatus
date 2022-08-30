857492ea5ed236784fd37ed82e8495fae55ef1b1
gamecattt <ak19gamecat@gmail.com>
2022-08-30 23:43:03 +0300

    media

--- script.js
+++ script.js
@@ -8,12 +8,21 @@
 //     });
 // });
 
-document.querySelectorAll(".faq__item").forEach(function (faq__item) {
-    faq__item.querySelector(".faq__heading").addEventListener("click", function () {
-        if (faq__item.classList.contains("active")) {
-            faq__item.classList.remove("active");
-        } else {
-            faq__item.classList.add("active");
-        }
-    });
+document.querySelectorAll('.faq__item').forEach(function (faq__item) {
+  faq__item.querySelector('.faq__heading').addEventListener('click', function () {
+    if (faq__item.classList.contains('active')) {
+      faq__item.classList.remove('active');
+    } else {
+      faq__item.classList.add('active');
+    }
+  });
+});
+
+const menu = document.querySelector('.header__menu');
+document.querySelector('.burger').addEventListener('click', function () {
+  if (menu.classList.contains('active')) {
+    menu.classList.remove('active');
+  } else {
+    menu.classList.add('active');
+  }
 });
